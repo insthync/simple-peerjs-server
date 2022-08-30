f1bbc86c74b669b189dd2fcab1c3e7f85dc9ea0f
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-08-30 18:05:47 +0700

    Add codes

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const { PeerServer } = require('peer');
+
+require('dotenv').config();
+
+let port = process.env.PORT ? Number(process.env.SSL_CERT_FILE_PATH) : 9000;
+let ssl = undefined;
+if (process.env.SSL_KEY_FILE_PATH && process.env.SSL_CERT_FILE_PATH) {
+    ssl = {
+        key: fs.readFileSync(process.env.SSL_KEY_FILE_PATH),
+        cert: fs.readFileSync(process.env.SSL_CERT_FILE_PATH)
+    }
+}
+console.log("Running peerjs server at port: " + port);
+const peerServer = PeerServer({
+    port,
+    ssl,
+});
